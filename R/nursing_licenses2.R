17d89ceae65d4e9f30b175f181c1dd1782974e63
Austin Fast <austinfast.news@gmail.com>
2024-07-21 10:49:24 -0400

    Removed AZBN scapre

--- R/nursing_licenses2.R
+++ R/nursing_licenses2.R
@@ -9,37 +9,38 @@ library(xml2)
 Sys.setenv(TZ="America/Los_Angeles")
 
 #Pull in AzBN numbers
-url <- "https://www.azbn.gov"
+#removed 7/21/24 -- results in 403 forbidden
+#url <- "https://www.azbn.gov"
 
-azbn <- read_html (url)
-azbn2 <- azbn %>%
-  html_nodes (xpath='//*[@id="block-nursestats-2"]/div/div/div/div')
+#azbn <- read_html (url)
+#azbn2 <- azbn %>%
+#  html_nodes (xpath='//*[@id="block-nursestats-2"]/div/div/div/div')
 
-azbn2 %>% html_nodes("ul") -> uls
-az_df <- data.frame()
-for (ul in uls) {
-  ul %>% html_text("li") -> text
-  type = gsub("\\d*,\\d*|\\d*", '', text)
-  count <- parse_number(text)
-  tmpdf <- data.frame(type,count) %>%
-    add_column (archive_date = Sys.Date())
-  az_df <- rbind(az_df,tmpdf)
-}
+#azbn2 %>% html_nodes("ul") -> uls
+#az_df <- data.frame()
+#for (ul in uls) {
+#  ul %>% html_text("li") -> text
+#  type = gsub("\\d*,\\d*|\\d*", '', text)
+#  count <- parse_number(text)
+#  tmpdf <- data.frame(type,count) %>%
+#    add_column (archive_date = Sys.Date())
+#  az_df <- rbind(az_df,tmpdf)
+#}
 
 #write AzBN license details
-write_csv (az_df, paste0("AzBN_scrapes/AzBN_", Sys.Date(), ".csv"))
+#write_csv (az_df, paste0("AzBN_scrapes/AzBN_", Sys.Date(), ".csv"))
 
 #Scrape active nursing license data from National Council of State Boards of Nursing
 
 #Scrape RN data from https://www.ncsbn.org/6161.htm
-url1 <- "https://www.ncsbn.org/Aggregate-RNActiveLicensesMap.html"
+url1 <- "https://www.ncsbn.org/NND/Maps/Aggregate-RNActiveLicensesMap.html"
 map_rn <- read_html(url1) %>%
   html_text()
 last_updated_rn = gsub(".*Last updated\\(", '', map_rn)
 last_updated_rn = mdy( gsub("\\).*", '', last_updated_rn) )
 
 #Read FusionChart data pulled from XML URL in map above
-map2_rn <- xml2::read_xml("https://www.ncsbn.org/Aggregate-RNActiveLicensesMap.xml")
+map2_rn <- xml2::read_xml("https://www.ncsbn.org/NND/Maps/Aggregate-RNActiveLicensesMap.xml")
 
 counts_rn <- map2_rn %>%
   xml_nodes('entity') %>%
@@ -62,14 +63,15 @@ df_rn <- data.frame(states_rn, counts_rn) %>%
 write_csv(df_rn, paste0("NCSBN_scrapes/RN_scrape_", Sys.Date(), ".csv"))
 
 #Scrape LPN data from https://www.ncsbn.org/6162.htm
-url2 <- "https://www.ncsbn.org/Aggregate-LPNActiveLicensesMap.html"
+#url2 <- "https://www.ncsbn.org/Aggregate-LPNActiveLicensesMap.html"
+url2 <- "https://www.ncsbn.org/NND/Maps/Aggregate-LPNActiveLicensesMap.html"
 map_lpn <- read_html(url2) %>%
   html_text()
 last_updated_lpn = gsub(".*Last updated\\(", '', map_lpn)
 last_updated_lpn = mdy( gsub("\\).*", '', last_updated_lpn) )
 
 #Read FusionChart data pulled from XML URL in map above
-map2_lpn <- xml2::read_xml("https://www.ncsbn.org/Aggregate-LPNActiveLicensesMap.xml")
+map2_lpn <- xml2::read_xml("https://www.ncsbn.org/NND/Maps/Aggregate-LPNActiveLicensesMap.xml")
 
 counts_lpn <- map2_lpn %>%
   xml_nodes('entity') %>%
@@ -92,14 +94,14 @@ df_lpn <- data.frame(states_lpn, counts_lpn) %>%
 write_csv(df_lpn, paste0("NCSBN_scrapes/LPN_scrape_", Sys.Date(), ".csv"))
 
 #Scrape ALL license data from https://www.ncsbn.org/Aggregate-AllActiveLicensesMap.html
-url3 <- "https://www.ncsbn.org/Aggregate-AllActiveLicensesMap.html"
+url3 <- "https://www.ncsbn.org/NND/Maps/Aggregate-AllActiveLicensesMap.html"
 map_all <- read_html(url3) %>%
   html_text()
 last_updated_all = gsub(".*Last updated\\(", '', map_all)
 last_updated_all = mdy( gsub("\\).*", '', last_updated_all) )
 
 #Read FusionChart data pulled from XML URL in map above
-map_all <- read_xml("https://www.ncsbn.org/Aggregate-AllActiveLicensesMap.xml")
+map_all <- read_xml("https://www.ncsbn.org/NND/Maps/Aggregate-AllActiveLicensesMap.xml")
 
 counts_all <- map_all %>%
   xml_nodes('entity') %>%
